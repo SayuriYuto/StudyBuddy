fe5879ef5357578c8b3dfc0cc42866a8bf2733db
Amey Dhuri <88235763+SayuriYuto@users.noreply.github.com>
2022-06-14 22:45:36 +0530

    Added OLED things, RTC

--- StandCode/StandCode.ino
+++ StandCode/StandCode.ino
@@ -1,55 +1,267 @@
-
-/**
- * Created by K. Suwatchai (Mobizt)
- *
- * Email: k_suwatchai@hotmail.com
- *
- * Github: https://github.com/mobizt/Firebase-ESP-Client
- *
- * Copyright (c) 2022 mobizt
- *
- */
-
-// This example shows how to create a document in a document collection. This operation required Email/password, custom or OAUth2.0 authentication.
-
-// #if defined(ESP32)
-// #include <WiFi.h>
-// #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
-// #endif
-
 #include <Firebase_ESP_Client.h>
-
-// Provide the token generation process info.
 #include <addons/TokenHelper.h>
 
-/* 1. Define the WiFi credentials */
 #define WIFI_SSID "ChotaBheem"
 #define WIFI_PASSWORD "kandalassan"
 
-/* 2. Define the API Key */
 #define API_KEY "AIzaSyDvpW0XKI2CF5fgdeGaltkZ9nOtTtSIHhs"
-
-/* 3. Define the project ID */
 #define FIREBASE_PROJECT_ID "studybuddytimer"
-
-/* 4. Define the user Email and password that alreadey registerd or added in your project */
 #define USER_EMAIL "amey.dhuri21@vit.edu"
 #define USER_PASSWORD "qwerty12345"
-
-// Define Firebase Data object
 FirebaseData fbdo;
-
 FirebaseAuth auth;
 FirebaseConfig config;
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1    // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 unsigned long dataMillis = 0;
 int count = 0;
 unsigned long sendDataPrevMillis = 0;
+unsigned long lastFBcheck = 0;
+bool timermode = LOW;
 
 #define switchPin D5
 int prevState = LOW;
 #define LED_BUILTIN 2
+
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+void studybuddytext();
+void wifibar(bool flag);
+void firebaseConnected(bool flag);
+void showclock(char timec[], bool flag); 
+void studying(bool flag, char timec[]);
+
+void setup()
+{
+
+    Serial.begin(115200);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
+    display.clearDisplay();
+    display.display();
+    studybuddytext();
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        wifibar(LOW);
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+    /* Assign the api key (required) */
+    config.api_key = API_KEY;
+
+    /* Assign the user sign in credentials */
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+
+    /* Assign the callback function for the long running token generation task */
+    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+    Firebase.begin(&config, &auth);
+
+    Firebase.reconnectWiFi(true);
+
+    // For sending payload callback
+    // config.cfs.upload_callback = fcsUploadCallback;
+    pinMode(switchPin, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    timeClient.begin();
+    timeClient.setTimeOffset(9000);
+}
+
+void loop()
+{
+    if (millis() - lastFBcheck > 1000)
+    {
+        if (WiFi.status() != WL_CONNECTED)
+        {
+            wifibar(LOW);
+            firebaseConnected(LOW);
+        }
+        else
+        {
+            wifibar(HIGH);
+            firebaseConnected(HIGH);
+        }
+        if (timermode == HIGH)
+        {
+            char pp[5];
+            //      int hourspassed = (sendDataPrevMillis)/3600000;
+            int minutespassed = (millis() - sendDataPrevMillis) / 60000;
+            int secondspassed = ((millis() - sendDataPrevMillis)-(minutespassed*60000)) / 1000;
+            sprintf(pp, "%02d:%02d", minutespassed, secondspassed);
+//            Serial.println(tt);
+            showclock(pp, HIGH);
+            
+            timeClient.update();
+            char tt[5];
+            sprintf(tt, "%02d:%02d", timeClient.getHours(), timeClient.getMinutes());
+            studying(HIGH, tt);
+        }
+        else
+        {
+            timeClient.update();
+            char tt[5];
+            sprintf(tt, "%02d:%02d", timeClient.getHours(), timeClient.getMinutes());
+            //      Serial.println(tt);
+            showclock(tt, HIGH);
+            studying(LOW, tt);
+        }
+        lastFBcheck = millis();
+    }
+
+    FirebaseJson content;
+    if (digitalRead(switchPin) == LOW && prevState == HIGH)
+    {
+        Serial.println("Button was pressed");
+//        studying(HIGH);
+        sendDataPrevMillis = millis();
+        Serial.println(millis());
+        digitalWrite(LED_BUILTIN, LOW);
+        timermode = HIGH;
+        prevState = LOW;
+    }
+
+    //    Button unpressed
+    if (digitalRead(switchPin) == HIGH && prevState == LOW)
+    {
+        Serial.println("Button is unpressed");
+        Serial.println(sendDataPrevMillis);
+        Serial.println(millis() - sendDataPrevMillis);
+        if ((millis() - sendDataPrevMillis > 15000) && Firebase.ready() && sendDataPrevMillis != 0)
+        {
+            String documentPath = "History/" + String(millis());
+            content.set("fields/time/integerValue", String((millis() - sendDataPrevMillis) / 1000));
+
+            String doc_path = "projects/";
+            doc_path += FIREBASE_PROJECT_ID;
+            doc_path += "/databases/(default)/documents/coll_id/doc_id";
+
+            if (Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "" /* databaseId can be (default) or empty */, documentPath.c_str(), content.raw()))
+                Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
+            else
+                Serial.println(fbdo.errorReason());
+            Serial.println("Set bool... OK Button unpressed");
+            digitalWrite(LED_BUILTIN, HIGH);
+        }
+        prevState = HIGH;
+        sendDataPrevMillis = 0;
+        timermode = LOW;
+    }
+}
+
+void wifibar(bool flag)
+{
+    if (flag == HIGH)
+    {
+        //  Serial.println("Yeswifibar");
+        display.fillRect(120, 0, 7, 9, BLACK);
+        display.drawRect(126, 1, 1, 8, WHITE);
+        display.drawRect(124, 3, 1, 6, WHITE);
+        display.drawRect(122, 5, 1, 4, WHITE);
+        display.drawRect(120, 7, 1, 2, WHITE);
+        display.display();
+    }
+    else
+    {
+        display.fillRect(120, 0, 7, 9, BLACK);
+        display.drawRect(124, 1, 1, 6, WHITE);
+        display.drawPixel(124, 8, WHITE);
+        display.display();
+    }
+}
+
+void firebaseConnected(bool flag)
+{
+    if (flag == HIGH)
+    {
+        //  Serial.println("Firebase connected");
+        display.fillRect(108, 1, 11, 8, BLACK);
+        display.setTextColor(WHITE);
+        display.setTextSize(1);
+        display.setCursor(108, 2);
+        display.println("FB");
+        display.display();
+    }
+    else
+    {
+        display.fillRect(108, 1, 11, 8, BLACK);
+    }
+}
+
+void studybuddytext()
+{
+    display.fillRect(0, 0, 58, 7, BLACK);
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.setCursor(1, 1);
+    display.println("STUDY BUDDY");
+    display.display();
+}
+
+void showclock(char timec[], bool flag) //flag useless
+{
+    if (flag == HIGH)
+    {
+        display.fillRect(7, 20, 125, 28, BLACK);
+        display.setTextColor(WHITE);
+        display.setTextSize(4);
+        display.setCursor(7, 20);
+        display.println(timec);
+        display.display();
+    }
+    else
+    {
+        display.fillRect(7, 20, 125, 28, BLACK);
+        display.display();
+    }
+}
+
+void studying(bool flag, char times[])
+{
+    if (flag == LOW)
+    {
+        display.fillRect(0, 56, 128, 5, BLACK);
+        display.setTextColor(WHITE);
+        display.setTextSize(1);
+        display.setCursor(24, 56);
+        display.println("Start Focusing!!");
+        display.display();
+    }
+    else
+    {
+        display.fillRect(0, 56, 128, 8, BLACK);
+        display.setTextColor(WHITE);
+        display.setTextSize(1);
+        display.setCursor(51, 56);
+        display.println(times);
+        display.display();
+    }
+}
 
 // The Firestore payload upload callback function
 void fcsUploadCallback(CFS_UploadStatusInfo info)
@@ -73,86 +285,5 @@ void fcsUploadCallback(CFS_UploadStatusInfo info)
     else if (info.status == fb_esp_cfs_upload_status_error)
     {
         Serial.printf("Upload failed, %s\n", info.errorMsg.c_str());
-    }
-}
-
-void setup()
-{
-
-    Serial.begin(115200);
-
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(300);
-    }
-    Serial.println();
-    Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
-    Serial.println();
-
-    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
-
-    /* Assign the api key (required) */
-    config.api_key = API_KEY;
-
-    /* Assign the user sign in credentials */
-    auth.user.email = USER_EMAIL;
-    auth.user.password = USER_PASSWORD;
-
-    /* Assign the callback function for the long running token generation task */
-    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-
-    Firebase.begin(&config, &auth);
-
-    Firebase.reconnectWiFi(true);
-
-    // For sending payload callback
-    // config.cfs.upload_callback = fcsUploadCallback;
-    pinMode(switchPin, INPUT);
-    pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void loop()
-{
-        FirebaseJson content;
-        //        Switch is pressed
-        // Serial.println(digitalRead(switchPin));
-        if (digitalRead(switchPin) == LOW && prevState == HIGH)
-        {
-            Serial.println("Button was pressed");
-            sendDataPrevMillis = millis();
-            Serial.println(millis());
-            digitalWrite(LED_BUILTIN, LOW);
-            prevState = LOW;
-        }
-
-    //    Button unpressed
-    if (digitalRead(switchPin) == HIGH && prevState == LOW)
-    {
-        Serial.println("Button is unpressed");
-        Serial.println(sendDataPrevMillis);
-        Serial.println(millis() - sendDataPrevMillis);
-        if ((millis() - sendDataPrevMillis > 15000)&&Firebase.ready()&&sendDataPrevMillis!=0)
-        {
-            String documentPath = "History/" + String(millis());
-            content.set("fields/time/integerValue", String((millis() - sendDataPrevMillis)/1000));
-            
-            String doc_path = "projects/";
-            doc_path += FIREBASE_PROJECT_ID;
-            doc_path += "/databases/(default)/documents/coll_id/doc_id";
-            
-            if (Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "" /* databaseId can be (default) or empty */, documentPath.c_str(), content.raw()))
-                Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
-            else
-                Serial.println(fbdo.errorReason());
-            Serial.println("Set bool... OK Button unpressed");
-            digitalWrite(LED_BUILTIN, HIGH);
-            
-        }
-        prevState = HIGH;
-        sendDataPrevMillis = 0;
     }
 }
